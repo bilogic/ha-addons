18cb23e28ac8b4410b95681546dd5507ffc046c5
bilogic <946010+bilogic@users.noreply.github.com>
2023-10-25 21:35:12 +0800

    navidrome

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ha-addons
+
+## References
+- https://github.com/alexbelgium/hassio-addons/issues/789
+- https://developers.home-assistant.io/docs/add-ons/tutorial/
+- https://blog.michalp.net/posts/smarthome/home-assistant-addons/
+- https://www.linkedin.com/pulse/creating-your-first-home-assistant-add-on-issac-goldstand/
+- https://github.com/alexbelgium/hassio-addons/wiki/Building-an-addon
+- https://github.com/edmundhighcock/hassio-repository/
---
+++ navidrome/rootfs/etc/cont-init.d/90-data_location.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bashio
+# shellcheck shell=bash
+# shellcheck disable=SC2155
+set -e
+
+# Configure app
+
+bashio::config.has_value 'ND_MUSICFOLDER' && export ND_MUSICFOLDER=$(bashio::config 'ND_MUSICFOLDER') && bashio::log.info "ND_MUSICFOLDER set to $ND_MUSICFOLDER"
+bashio::config.has_value 'ND_DATAFOLDER' && export ND_DATAFOLDER=$(bashio::config 'ND_DATAFOLDER') && bashio::log.info "ND_DATAFOLDER set to $ND_DATAFOLDER"
+
+# Check data location
+LOCATION=$(bashio::config 'ND_DATAFOLDER')
+if !(bashio::config.has_value 'ND_DATAFOLDER'); then
+    bashio::log.warning "No ND_DATAFOLDER location was provided."
+    exit 1
+fi
+
+# Create folder
+if [ ! -d "$ND_DATAFOLDER" ]; then
+    bashio::log.info "Creating ND_DATAFOLDER $ND_DATAFOLDER"
+    mkdir -p "$ND_DATAFOLDER"
+fi
+
+##############
+# LAUNCH APP #
+##############
+
+## We are in the container's FS here
+bashio::log.info "Looking inside /data"
+ls -la /data
+
+bashio::log.info "Looking inside /music"
+ls -la /music
+
+bashio::log.info "Looking inside ND_DATAFOLDER $ND_DATAFOLDER"
+ls -la $ND_DATAFOLDER
+
+bashio::log.info "Looking inside ND_MUSICFOLDER $ND_MUSICFOLDER"
+ls -la $ND_MUSICFOLDER
+
+bashio::log.info 'Starting Navidrome...'
+/app/navidrome
